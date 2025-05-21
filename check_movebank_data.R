ab10f96234cc9b5d46a77e882d38e6d925c26a2d
Ariana Strandburg-Peshkin <ari@kn-eas-m019.local>
2025-05-21 17:32:36 +0000

    small changes to plotting and text output

--- check_movebank_data.R
+++ check_movebank_data.R
@@ -8,7 +8,7 @@ gpsdat_file <- '~/Dropbox/Teaching/VTK_2025/mpala_data_2025/Baboons MBRP Mpala K
 #path to file containing metadata from movebank
 metadata_file <- '~/Dropbox/Teaching/VTK_2025/mpala_data_2025/Baboons MBRP Mpala Kenya-reference-data.csv'
 
-date_to_check <- '2025-05-07'
+date_to_check <- '2025-05-05'
 
 highres_hrs <- c(3,4)
 
@@ -32,6 +32,8 @@ dat <- alldat[which(alldat$tag.local.identifier %in% tag_ids & date(alldat$times
 
 #how much data is there per hour?
 if(nrow(dat)>0){
+  print(paste('Fixes per hour on date', date_to_check, 'for group', group_to_check,':'))
+  print(table(hour(dat$timestamp)))
   plot(table(hour(dat$timestamp)), xlab = 'hour', ylab = 'Fixes', main = 'Data present per hour (all individuals in group)')
 } else{
   print(paste0('No data for group ', group_to_check, ' on date ', date_to_check))
@@ -40,7 +42,12 @@ if(nrow(dat)>0){
 dat_highres_period <- dat[which(hour(dat$timestamp) %in% highres_hrs),]
 
 if(nrow(dat_highres_period)>0){
-  plot(table(dat_highres_period$tag.local.identifier), xlab = 'tag id', ylab = 'Fixes', main = 'Data present during highres interval')
+  fixes_per_ind <- table(dat_highres_period$tag.local.identifier)
+  plot(NULL, xlab = 'tag id', ylab = 'Fixes', xlim = c(1,length(fixes_per_ind)), ylim = c(0, max(fixes_per_ind)), main = 'Data present during highres interval', xaxt='n')
+  arrows(1:length(fixes_per_ind), fixes_per_ind, 1:length(fixes_per_ind), rep(0, length(fixes_per_ind)), length = 0)
+  axis(1, at = 1:length(fixes_per_ind), names(fixes_per_ind),las=2)
+  print("Fixes per individual during high res period:")
+  print(fixes_per_ind)
 } else{
   print(paste0('No data for group ', group_to_check, ' on date ', date_to_check, ' during the high res hours ', paste(highres_hrs, collapse = ',')))
 }
